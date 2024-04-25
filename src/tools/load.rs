b89358425a6961c47b888864c3481bb08562c4fe
Hank Lewis <hank@modernatomic.com>
2024-04-25 13:55:28 -0500

    Added load binary and README.adoc

---
+++ src/tools/load.rs
@@ -0,0 +1,30 @@
+use std::{fs, io, env};
+
+fn main() -> anyhow::Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        anyhow::bail!({
+            io::Error::new(
+                io::ErrorKind::InvalidInput, 
+                "Expected a single argument \
+                (the path to a JSON-specified scene)."
+            )
+        });
+    }
+
+    let config = rt::Config {
+        resolution: rt::Resolution::Dynamic(16),
+        fps: 60,
+        ..Default::default()
+    };
+
+    let scene_reader = io::BufReader::new({
+        fs::File::open(&args[1])?
+    });
+
+    let scene: rt::scene::Scene = //
+        serde_json::from_reader(scene_reader)?;
+
+    pollster::block_on(rt::run_native(config, scene))
+}
